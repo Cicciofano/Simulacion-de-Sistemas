06a31cf0ab12c6ea5b9c32f5676de187c31963b8
Francisco Gerardo Meza Fierro <30807006+Cicciofano@users.noreply.github.com>
2017-08-13 15:49:28 -0500

    Update reto2.R

--- Tarea1/reto2.R
+++ Tarea1/reto2.R
@@ -1,6 +1,9 @@
+#esta sección del código se modificó para analizar el reto 2
+#se usó la función sapply para comparar el qué tan ineficiente es el no aprovechar paralelismo 
+
 for (dimension in 1:8)
 { 
   clusterExport(cluster, "dimension")
-  resultado <- parSapply(cluster, 1:repetir, experimento)
+  resultado <- sapply(1:repetir, experimento)
   datos <- rbind(datos, resultado)
 }
