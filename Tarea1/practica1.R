996e8a0bdc22df0f3a1c19a0083e43384f8cd23b
FGMF\Gerardo M <lmfgmf@hotmail.com>
2017-08-12 19:23:13 -0500

    no message

---
+++ Tarea1/practica1.R
@@ -0,0 +1,44 @@
+repetir <- 100
+
+pasos <- 200
+
+library(parallel)
+
+datos <-  data.frame()
+
+experimento <- function(replica) 
+  {
+   pos <- rep(0, dimension)
+   veces <- 0
+   for (t in 1:pasos) 
+     {
+      cambiar <- sample(1:dimension, 1)
+      cambio <- 1
+      if (runif(1) < 0.5) 
+        {
+         cambio <- -1
+        }
+      pos[cambiar] <- pos[cambiar] + cambio
+      if (all(pos == 0)) 
+        {
+         veces <- veces + 1
+        }
+     }
+   return(veces)
+  }
+
+cluster <- makeCluster(detectCores() - 1)
+clusterExport(cluster, "pasos")
+clusterExport(cluster, "experimento")
+
+for (dimension in 1:8)
+  { 
+   clusterExport(cluster, "dimension")
+   resultado <- parSapply(cluster, 1:repetir, experimento)
+   datos <- rbind(datos, resultado)
+  } 
+
+stopCluster(cluster)
+
+boxplot(data.matrix(datos), use.cols=FALSE, xlab="Dimensión", ylab="Número de regresos al origen", main="Regresos al origen por dimensión")
+
